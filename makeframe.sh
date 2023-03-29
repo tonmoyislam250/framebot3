d791b1048b035f607e9a5237a4341a8aac394744
Tonmoy <127471199+tonmoyislam12@users.noreply.github.com>
2023-03-29 18:27:41 +0600

    Update makeframe.sh

--- makeframe.sh
+++ makeframe.sh
@@ -1,1 +1,2 @@
-ffmpeg -copyts -i "Episode_01.mkv" -r 1000 -vf "mpdecimate=hi=64*12*15:lo=64*5*15:frac=1",subtitles=sub1.ass -frame_pts true -vsync vfr -q:v 5 "Sub_01/%08d.jpg" -r 1000 -vf "mpdecimate=hi=64*12*15:lo=64*5*15:frac=1" -frame_pts true -vsync vfr -q:v 5 "Raw_01/%08d.jpg"
+VIDEODIR=./videos/episode2.mkv
+ffmpeg -copyts -i "$VIDEO_DIR" -r 1000 -vf "mpdecimate=hi=64*12*15:lo=64*5*15:frac=1",subtitles=sub1.ass -frame_pts true -vsync vfr -q:v 5 "Sub_01/%08d.jpg" -r 1000 -vf "mpdecimate=hi=64*12*15:lo=64*5*15:frac=1" -frame_pts true -vsync vfr -q:v 5 "Raw_01/%08d.jpg"
