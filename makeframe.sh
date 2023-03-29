446205b6939c5529e85eb1925107759ee719bb05
Tonmoy <127471199+tonmoyislam12@users.noreply.github.com>
2023-03-29 19:46:07 +0600

    Update makeframe.sh

--- makeframe.sh
+++ makeframe.sh
@@ -5,6 +5,7 @@ VIDEO_DIR= "./videos"
 VIDEO_NAME= "Ben 10 Alien Force S01E02 Ben 10 Returns (2).mkv"
 cd /usr/src/app
 gdown $LINK
-mkdir -pv videos && unzip -d ./videos/ -j Ben10AFS01.zip
+file ben10AFS01.zip
+mkdir -pv videos && unzip -d ./videos/ -j ben10AFS01.zip
 ls -a
 ffmpeg -copyts -i "$VIDEO_DIR/$VIDEO_NAME" -r 1000 -vf "mpdecimate=hi=64*12*15:lo=64*5*15:frac=1",subtitles=sub1.ass -frame_pts true -vsync vfr -q:v 5 "Sub_01/%08d.jpg" -r 1000 -vf "mpdecimate=hi=64*12*15:lo=64*5*15:frac=1" -frame_pts true -vsync vfr -q:v 5 "Raw_01/%08d.jpg"
