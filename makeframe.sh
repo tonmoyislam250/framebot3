4a05d94e4a4813cff1cae8333f6ed7d37fb9fc01
Tonmoy <127471199+tonmoyislam12@users.noreply.github.com>
2023-03-29 23:06:24 +0600

    Update makeframe.sh

--- makeframe.sh
+++ makeframe.sh
@@ -6,5 +6,5 @@ gdown $LINK
 file ben10AFS01.zip
 mkdir -pv videos/raw && unzip -d ./videos/ -j ben10AFS01.zip
 ls -a
-ffmpeg -copyts -i "./videos/${VIDEO_NAME}" -r 1000 -vf "mpdecimate=hi=64*12:lo=64*5:frac=0.33" -frame_pts true -vsync vfr -q:v 1 "./videos/raw/%04d.jpg"
+ffmpeg -copyts -i "./videos/${VIDEO_NAME}" -r 1000 -vf "mpdecimate=hi=64*12*15:lo=64*5*15:frac=1" -frame_pts true -vsync vfr -q:v 5 "./videos/raw/%08d.jpg"
 python3 yafot-facebook.py --page-id $PAGE_ID --pdir "videos/raw/" --token $ACCESS_TOKEN --start $START --count 5000 --delay 60
