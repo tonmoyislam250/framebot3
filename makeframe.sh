3cf4d80fbb7ac44e714a66d5c85cf41092ae1958
Tonmoy <127471199+tonmoyislam12@users.noreply.github.com>
2023-03-29 19:57:10 +0600

    Update makeframe.sh

--- makeframe.sh
+++ makeframe.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 pwd && ls -a
 LINK="https://drive.google.com/uc?id=1szSZvOG9vXj93C0WmCszEHP6-C3jG4sh"
-VIDEO_NAME= "Ben 10 Alien Force S01E02 Ben 10 Returns (2).mkv"
 cd /usr/src/app
 gdown $LINK
 file ben10AFS01.zip
 mkdir -pv videos/raw && unzip -d ./videos/ -j ben10AFS01.zip
 ls -a
-ffmpeg -copyts -i "./videos/$VIDEO_NAME" -r 1000 -vf "mpdecimate=hi=64*12*15:lo=64*5*15:frac=1" -frame_pts true -vsync vfr -q:v 5 "./videos/raw/%04d.jpg"
+ffmpeg -copyts -i "./videos/Ben 10 Alien Force S01E02 Ben 10 Returns (2).mkv" -r 1000 -vf "mpdecimate=hi=64*12*15:lo=64*5*15:frac=1" -frame_pts true -vsync vfr -q:v 5 "./videos/raw/%04d.jpg"
