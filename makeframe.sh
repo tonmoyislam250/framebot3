248c105eb5ee2ee116f7ef3c0d2e4aa0009bf5b8
Tonmoy <127471199+tonmoyislam12@users.noreply.github.com>
2023-04-01 04:32:47 +0600

    Update makeframe.sh

--- makeframe.sh
+++ makeframe.sh
@@ -11,8 +11,5 @@ ffmpeg -sub_charenc $(uchardet videos/${VIDEO_NAME}.srt) -i videos/${VIDEO_NAME}
 ffmpeg -copyts -i \
        "./videos/${VIDEO_NAME}.mp4" \
        -r 1000 -vf "mpdecimate=hi=64*12*15:lo=64*5*15:frac=1",subtitles=videos/${VIDEO_NAME}.ass \
-       -frame_pts true -vsync vfr -q:v 5 "videos/sub/%08d.jpg" \
-       -r 1000 -vf "mpdecimate=hi=64*12*15:lo=64*5*15:frac=1" -frame_pts true \
-       -vsync vfr -q:v 5 "videos/raw/%08d.jpg"
-
+       -frame_pts true -vsync vfr -q:v 5 "videos/sub/%08d.jpg"
 python3 yafot-facebook.py --page-id $PAGE_ID --pdir "videos/sub/" --token $ACCESS_TOKEN --start $START --count 1800 --delay 60
