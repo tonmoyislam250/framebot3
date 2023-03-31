c4b6a49b0236414c211162befd3b5a45a2dd4ece
Tonmoy <127471199+tonmoyislam12@users.noreply.github.com>
2023-04-01 03:55:34 +0600

    Create makeframe.sh

--- makeframe.sh
+++ makeframe.sh
@@ -10,7 +10,7 @@ rm -rf videos/raw/* && rm -rf videos/sub/*
 ffmpeg -copyts -i \
        "./videos/${VIDEO_NAME}.mp4" \
        -r 1000 -vf "mpdecimate=hi=64*12*15:lo=64*5*15:frac=1", \
-       subtitles=${VIDEONAME}.srt -frame_pts true -vsync vfr -q:v 5 "videos/sub/%08d.jpg" \
+       subtitles=${VIDEO_NAME}.srt -frame_pts true -vsync vfr -q:v 5 "videos/sub/%08d.jpg" \
        -r 1000 -vf "mpdecimate=hi=64*12*15:lo=64*5*15:frac=1" -frame_pts true \
        -vsync vfr -q:v 5 "videos/raw/%08d.jpg"
 
