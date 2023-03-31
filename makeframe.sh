cd4b6d35946ea34ed4c8864a18b97636bea97566
Tonmoy <127471199+tonmoyislam12@users.noreply.github.com>
2023-04-01 04:00:37 +0600

    Update makeframe.sh

--- makeframe.sh
+++ makeframe.sh
@@ -5,12 +5,12 @@ gdown $LINK
 mkdir -pv videos/sub
 mkdir -pv videos/raw \
 && unzip -d ./videos/ -j ${VIDEO_NAME}.zip
-ls -a videos/sub
+ls -a videos/sub && ls -a videos
 rm -rf videos/raw/* && rm -rf videos/sub/*
 ffmpeg -copyts -i \
        "./videos/${VIDEO_NAME}.mp4" \
-       -r 1000 -vf "mpdecimate=hi=64*12*15:lo=64*5*15:frac=1", \
-       subtitles=${VIDEO_NAME}.srt -frame_pts true -vsync vfr -q:v 5 "videos/sub/%08d.jpg" \
+       -r 1000 -vf "mpdecimate=hi=64*12*15:lo=64*5*15:frac=1",subtitles=${VIDEO_NAME}.srt \
+       -frame_pts true -vsync vfr -q:v 5 "videos/sub/%08d.jpg" \
        -r 1000 -vf "mpdecimate=hi=64*12*15:lo=64*5*15:frac=1" -frame_pts true \
        -vsync vfr -q:v 5 "videos/raw/%08d.jpg"
 
