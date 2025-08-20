db37fbfd10b7eb442727fb535abef39d906a2ab7
tonmoyislam250 <tonmoyk983@gmail.com>
2025-08-20 11:57:22 +0600

    Update .gitignore to include .env, .mkv, .mp4, and RAW/; modify frame.sh to create RAW directory and adjust output paths

--- frame.sh
+++ frame.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 pwd && ls -a
 cd /usr/src/app
+mkdir RAW
 gdown $LINK
 # mkdir -pv videos/sub
 # mkdir -pv videos/raw \
@@ -10,6 +11,5 @@ gdown $LINK
 # ffmpeg -sub_charenc $(uchardet videos/${VIDEO_NAME}.srt) -i videos/${VIDEO_NAME}.srt videos/${VIDEO_NAME}.ass
 ffmpeg -copyts -i \
        "./${VIDEO_NAME}.mp4" \
-       -r 1000 -vf "mpdecimate=hi=64*12*15:lo=64*5*15:frac=1" \
-       -frame_pts true -vsync vfr -q:v 5 "videos/sub/%08d.jpg"
-python3 post.py --page-id $PAGE_ID --pdir "videos/sub/" --token $ACCESS_TOKEN --start $START --count 1800 --delay 60
+       -vf "mpdecimate=hi=64*12*15:lo=64*5*15:frac=1" -frame_pts true -vsync vfr -q:v 5 "RAW/%08d.jpg"
+python3 post.py --page-id $PAGE_ID --pdir "RAW/" --token $ACCESS_TOKEN --start $START --count 1800 --delay 60
