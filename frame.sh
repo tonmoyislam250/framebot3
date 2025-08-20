6cf1f58a7299699217b9283c443d304f283aad80
tonmoyislam250 <tonmoyk983@gmail.com>
2025-08-20 12:07:12 +0600

    Update frame.sh to change input video format from .mp4 to .mkv

--- frame.sh
+++ frame.sh
@@ -10,6 +10,6 @@ gdown $LINK
 # rm -rf videos/raw/* && rm -rf videos/sub/*
 # ffmpeg -sub_charenc $(uchardet videos/${VIDEO_NAME}.srt) -i videos/${VIDEO_NAME}.srt videos/${VIDEO_NAME}.ass
 ffmpeg -copyts -i \
-       "./${VIDEO_NAME}.mp4" \
+       "./${VIDEO_NAME}.mkv" \
        -vf "mpdecimate=hi=64*12*15:lo=64*5*15:frac=1" -frame_pts true -vsync vfr -q:v 5 "RAW/%08d.jpg"
 python3 post.py --page-id $PAGE_ID --pdir "RAW/" --token $ACCESS_TOKEN --start $START --count 1800 --delay 60
